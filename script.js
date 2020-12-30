e7b5428c8a198ebab88583198342bdb7aa2b8937
marshmalow212 <erfanultaher@yahoo.com>
2020-12-30 12:28:17 +0600

    day 2

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+var portfolio_link = document.getElementsByClassName("navbar-brand");
+
+var home_lnk = document.getElementById("home_li");
+
+$(portfolio_link).on('click', function(){
+    $('li').removeClass('active');
+    $(home_lnk).toggleClass('active');
+});
+
+
 $('li').on('click', function(){
-    $('li').removeclass('active'),
-    $(this).toggleclass('active')
-})
+    $('li').removeClass('active');
+    $(this).toggleClass('active');
+});
+
+
+var search_btn = document.getElementById("ssbtn");
+
+$(search_btn).on('click',function(){
+    var search_bar_text = document.getElementById("sbar").value;
+    console.log(search_bar_text);
+    $(document.getElementById("sbar").value=null);
+});
